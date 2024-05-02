30c68e2ca8f4e52a54a3fccafc12c87cdedc6fe7
drobzin <makspom74@gmail.com>
2024-05-02 18:31:19 +1000

    чето сделал)

--- react_app/src/components/Navbar.jsx
+++ react_app/src/components/Navbar.jsx
@@ -5,16 +5,17 @@ import useAuth from "../hooks/use-auth";
 
 
 const Navbar = () => {
-const { isAuthenticated, username, logout } = useAuth();
+    const { isAuthenticated, username, logout } = useAuth();
     return (
         <div>
             <Link to="/">Home</Link>
+
             <Link to="/register">Register</Link>
             <Link to="/login">Login</Link>
-            <Link to ="addPlace">addPlace</Link>
+            <Link to="addPlace">addPlace</Link>
             <h1>{username}</h1>
             {isAuthenticated ? <button onClick={logout} >Logout</button> : <></>}
-            
+
         </div>)
 }
 export default Navbar
